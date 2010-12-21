8e2e1a78ebff395ff95dd0b22f663c82ebc57925
zhangyun2k8 <zhangyun2k8@ae84354e-72e1-f882-9202-20098cca1072>
2010-12-21 08:24:10 +0000

    # update xcode command for convert char to gui.

--- wiki/qadnote/xcode.cmd
+++ wiki/qadnote/xcode.cmd
@@ -2,13 +2,19 @@ rem @echo off
 set xrc=c:\qadguicli
 set xcode=D:\xcode
 del %xrc%\xrc\*.* /s/q/f/a
-rd %xrc%\xrc /s/q
+rd %xrc%\xrc
 mkdir %xrc%\xrc
 
-set src=D:\Trunk\sincere\sh005\xxworcmt
+rem Xcode.
+set src=D:\ss2k10\trunk\common
 cd /d %src%
-%xcode%\xcode -d %xrc%\xrc xxworc*.p
-%xcode%\xcode -d %xrc%\xrc xx*300.p
+%xcode%\xcode -d %xrc%\xrc *.p
 %xcode%\xcode -d %xrc%\xrc *.i
-copy /y lbl.cim + msg.cim %xrc%\xrc\lbl.cim
 
+rem convert.
+mkdir %xrc%\gui
+echo srcSearchPath=%src%> %xrc%\convert.tmp
+echo targetDir=%xrc%\gui>> %xrc%\convert.tmp
+echo workDir=%xrc%>> %xrc%\convert.tmp
+%xcode%\convert.exe -r -ini=%xrc%\convert.tmp %src%\xx*.p>outlog.log
+del %xrc%\convert.tmp /s/q/f/a
