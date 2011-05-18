90b6fc0580d2318f13875cfd7f0638acbdac04f6
ricenzhang <ricenzhang@ae84354e-72e1-f882-9202-20098cca1072>
2011-05-18 08:47:40 +0000

    # fix Cim2000.xla bug 备用字段不能正常导入.

--- wiki/qadnote/tools/ftp2server.Sendto.cmd
+++ wiki/qadnote/tools/ftp2server.Sendto.cmd
@@ -1,5 +1,6 @@
-rem 吧这个档案加入右键菜单可以方便上传到ftpserver.
+rem 把这个档案加入右键菜单可以方便上传到ftpserver.
 set xrc=C:\qadguicli
+set tpe=%1
 cd /d %xrc%\xrc
 rem echo open 222.85.150.27> %xrc%\~ftp.txt
 echo open 192.168.0.92> %xrc%\~ftp.txt
@@ -12,27 +13,7 @@ rem  echo cd /home/demo/xrc>> %xrc%\~ftp.txt
 echo cd /var/tmp/xrc>> %xrc%\~ftp.txt
 echo bin>> %xrc%\~ftp.txt
 rem echo prompt off>> %xrc%\~ftp.txt
-echo put %1>> %xrc%\~ftp.txt
-echo put %2>> %xrc%\~ftp.txt
-echo put %3>> %xrc%\~ftp.txt
-echo put %4>> %xrc%\~ftp.txt
-echo put %5>> %xrc%\~ftp.txt
-echo put %6>> %xrc%\~ftp.txt
-echo put %7>> %xrc%\~ftp.txt
-echo put %8>> %xrc%\~ftp.txt
-echo put %9>> %xrc%\~ftp.txt
-echo put %10>> %xrc%\~ftp.txt
-echo put %11>> %xrc%\~ftp.txt
-echo put %12>> %xrc%\~ftp.txt
-echo put %13>> %xrc%\~ftp.txt
-echo put %14>> %xrc%\~ftp.txt
-echo put %15>> %xrc%\~ftp.txt
-echo put %16>> %xrc%\~ftp.txt
-echo put %17>> %xrc%\~ftp.txt
-echo put %18>> %xrc%\~ftp.txt
-echo put %19>> %xrc%\~ftp.txt
+if %tpe:~-2,1%==. (echo put %1 >> %xrc%\~ftp.txt) else (if %tpe:~-3,1%==. (echo put %1 >> %xrc%\~ftp.txt) else (if %tpe:~-4,1%==. (echo put %1 >> %xrc%\~ftp.txt) else (echo mput %1\*.*>> %xrc%\~ftp.txt)))
 echo bye>> %xrc%\~ftp.txt
-
 ftp -i -s:%xrc%\~ftp.txt
 del /s/q/f/a %xrc%\~ftp.txt
-pause
