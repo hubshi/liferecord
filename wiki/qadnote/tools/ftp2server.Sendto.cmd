1c03154bfd024a61d07fd8452d14429ab1171f5b
ricenzhang <ricenzhang@ae84354e-72e1-f882-9202-20098cca1072>
2011-08-11 05:42:39 +0000

    # update cash_hist.xls.

--- wiki/qadnote/tools/ftp2server.Sendto.cmd
+++ wiki/qadnote/tools/ftp2server.Sendto.cmd
@@ -9,12 +9,21 @@ rem showa set vpasswd=plan2011
 rem showa set vfile=%1
 rem showa set vworkdir=c:\qadguicli
 
+rem set vlcd=c:\qadguicli\xrc
+rem set vcd=/var/tmp/xrc
+rem set vhostip=222.85.150.27
+rem set vusr=mfg
+rem set vpasswd=mfgmfg
+rem set vfile=c:\qadguicli\xrc
+rem set vworkdir=c:\qadguicli
+
+rem sinpo ftp parameter
 set vlcd=c:\qadguicli\xrc
-set vcd=/var/tmp
-set vhostip=192.168.20.9
+set vcd=/var/tmp/xrc
+set vhostip=222.85.150.27
 set vusr=mfg
-set vpasswd=
-set vfile=%1
+set vpasswd=mfgmfg
+set vfile=c:\qadguicli\xrc
 set vworkdir=c:\qadguicli
 
 cd /d %vlcd%
@@ -24,7 +33,7 @@ echo %vpasswd%>> %vworkdir%\~ftp.txt
 echo cd %vcd%>> %vworkdir%\~ftp.txt
 echo bin>> %vworkdir%\~ftp.txt
 rem echo prompt off>> %vworkdir%\~ftp.txt
-if %vfile:~-2,1%==. (echo put %1 >> %vworkdir%\~ftp.txt) else (if %vfile:~-3,1%==. (echo put %1 >> %vworkdir%\~ftp.txt) else (if %vfile:~-4,1%==. (echo put %1 >> %vworkdir%\~ftp.txt) else (echo mput %1\*.*>> %vworkdir%\~ftp.txt)))
+if %vfile:~-2,1%==. (echo put %vfile% >> %vworkdir%\~ftp.txt) else (if %vfile:~-3,1%==. (echo put %vfile% >> %vworkdir%\~ftp.txt) else (if %vfile:~-4,1%==. (echo put %vfile% >> %vworkdir%\~ftp.txt) else (echo mput %vfile%\*.*>> %vworkdir%\~ftp.txt)))
 echo bye>> %vworkdir%\~ftp.txt
 
 ftp -i -s:%vworkdir%\~ftp.txt
