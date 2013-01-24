7785b40f0818ed6b0f17fb2788d23f689350ac82
r7733251 <r7733251@ae84354e-72e1-f882-9202-20098cca1072>
2013-01-24 11:13:53 +0000

    # add to 右键列表。

--- wiki/qadnote/tools/xcode.cmd
+++ wiki/qadnote/tools/xcode.cmd
@@ -1,37 +1,49 @@
 rem @echo off
-set wkdir=c:\qadguicli
-set xcode=C:\qadguicli\proedit
+set fold="%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%%time:~9,2%"
+set vfile=%1
+rem set vfile=D:\ss\trunk\sinpo\xxarrp01
 set xrc=c:\qadguicli\xrc
+rem set xrc=\\192.168.0.92\admin\cim\xrc
 set src=C:\qadguicli\garbage\src
 set cim=C:\qadguicli\garbage\cim
-set gui=C:\qadguicli\garbage\gui
+set gui=c:\qadguicli\garbage\gui
+set wkdir=c:\qadguicli
+set xcode=C:\qadguicli\proedit
+cd /d c:\qadtuicli
+mkdir C:\qadguicli\garbage\%fold%\
+move %src%\* c:\qadguicli\garbage\%fold%\
+move %xrc%\* c:\qadguicli\garbage\%fold%\
+move %wkdir%\gui\* c:\qadguicli\garbage\%fold%\
 
-del %src%\*.* /s/q/f/a
-del %xrc%\*.* /s/q/f/a
-del %cim%\*.* /s/q/f/a
+rem del %src%\*.* /s/q/f/a
+rem del %xrc%\*.* /s/q/f/a
+rem del %cim%\*.* /s/q/f/a
+
 mkdir %src%
 mkdir %xrc%
-mkdir %cim%
 
-call %wkdir%\cpfile.cmd
+rem copy src to srcdir
+
+rem All program
+
+if %vfile:~-2,1%==. (copy /y %vfile% %src%\) else (if %vfile:~-3,1%==. (copy /y %vfile% %src%\) else (if %vfile:~-4,1%==. (copy /y %vfile% %src%\) else (copy /y %vfile%\*.* %src%\)))
 
 rem Xcode.
 cd /d %src%
 %xcode%\xcode -d %xrc% *.p
+%xcode%\xcode -d %xrc% *.w
 %xcode%\xcode -d %xrc% *.i
 %xcode%\xcode -d %xrc% *.v
 %xcode%\xcode -d %xrc% *.t
-
-copy %cim%\*.cim %xrc%\p.cim
-
-rem convert.
+rem if %2=="AA" goto exitme
+goto exitme
+rem convexitme
 mkdir %gui%
-if %1=="A" goto exitme
-echo srcSearchPath=%src%> %xrc%\convert.tmp
-echo targetDir=%xrc%\..\gui>> %xrc%\convert.tmp
-echo workDir=%xrc%>> %xrc%\convert.tmp
-%xcode%\convert.exe -r -ini=%xrc%\convert.tmp %src%\*.p>%xrc%\convert.log
-%xcode%\convert.exe -r -ini=%xrc%\convert.tmp %src%\*.i>>%xrc%\convert.log
-rem del %xrc%\convert.tmp /s/q/f/a
+echo srcSearchPath=%src%> %wkdir%\convert.tmp
+echo targetDir=%wkdir%\gui>> %wkdir%\convert.tmp
+echo workDir=%wkdir%>> %wkdir%\convert.tmp
+%xcode%\convert.exe -r -ini=%wkdir%\convert.tmp %src%\*.p>%wkdir%\convert.log
+%xcode%\convert.exe -r -ini=%wkdir%\convert.tmp %src%\*.i>>%wkdir%\convert.log
+rem del %wkdir%\convert.tmp /s/q/f/a
 
 :exitme
