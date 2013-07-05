84d1fb2de12f987a164e4f6a6595cbafd160bec9
r7733251 <r7733251@ae84354e-72e1-f882-9202-20098cca1072>
2013-07-05 07:31:15 +0000

    # expor convert dir.

--- wiki/qadnote/tools/conv/convertx64.bat
+++ wiki/qadnote/tools/conv/convertx64.bat
@@ -3,5 +3,6 @@ echo > %wkdir%\convert.log
 copy %1 %wkdir%\src
 cd /d %wkdir%
 for /r %%i in (.\src\*.p .\src\*.i) do %wkdir%\convx64.exe "%%i" >>%wkdir%\convert.log
+explorer "%wkdir%\gui"
 del /f/s/q %wkdir%\src\*.* 
 pause 
